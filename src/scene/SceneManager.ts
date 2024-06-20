bfe529f723be128a7d4b2831996192a479dea6d0
Ethan Gretna <pixelfrostypublic@gmail.com>
2024-06-20 19:46:38 -0300

    OrbitControls functional

--- src/scene/SceneManager.ts
+++ src/scene/SceneManager.ts
@@ -1,82 +1,85 @@
 // Code taken from repo textalive-app-dance
 import * as THREE from "three";
 import { Building } from "./Building";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 export class SceneManager {
-  private _view: HTMLElement;
+    private _view: HTMLElement;
 
-  private _renderer: THREE.WebGLRenderer;
-  private _scene: THREE.Scene;
-  private _camera: THREE.Camera;
-  // private geometry = new THREE.BoxGeometry(1, 1, 1);
-  // private material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  // private cube = new THREE.Mesh(this.geometry, this.material);
+    private _renderer: THREE.WebGLRenderer;
+    private _scene: THREE.Scene;
+    private _camera: THREE.Camera;
+    private _controls;
+    // private geometry = new THREE.BoxGeometry(1, 1, 1);
+    // private material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    // private cube = new THREE.Mesh(this.geometry, this.material);
 
-  constructor() {
-    this._scene = new THREE.Scene();
-    this._camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000,
-    );
+    constructor() {
+        this._view = document.querySelector("#view")!;
+        this._scene = new THREE.Scene();
+        this._camera = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000,
+        );
 
-    this._renderer = new THREE.WebGLRenderer();
+        this._renderer = new THREE.WebGLRenderer();
 
-    const light = new THREE.AmbientLight(0x202020);
-    this._scene.add(light);
+        const light = new THREE.AmbientLight(0x202020);
+        this._scene.add(light);
 
-    const plight = new THREE.PointLight(0xffffff, 10, 100);
-    plight.position.set(0, 2, 0);
-    plight.castShadow = true;
-    this._scene.add(plight);
+        const plight = new THREE.PointLight(0xffffff, 10, 100);
+        plight.position.set(0, 2, 0);
+        plight.castShadow = true;
+        this._scene.add(plight);
 
-    const planeGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
-    const planeMaterial = new THREE.MeshStandardMaterial({
-      color: 0x00ff00,
-      side: THREE.DoubleSide,
-    });
-    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.receiveShadow = true;
-    plane.rotation.x = Math.PI / 2;
-    this._scene.add(plane);
+        const planeGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
+        const planeMaterial = new THREE.MeshStandardMaterial({
+            color: 0x00ff00,
+            side: THREE.DoubleSide,
+        });
+        const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+        plane.receiveShadow = true;
+        plane.rotation.x = Math.PI / 2;
+        this._scene.add(plane);
 
-    this.resize();
-    // this.geometry = new THREE.BoxGeometry(1, 1, 1);
-    // this.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    // this.cube = new THREE.Mesh(this.geometry, this.material);
+        this.resize();
+        // this.geometry = new THREE.BoxGeometry(1, 1, 1);
+        // this.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+        // this.cube = new THREE.Mesh(this.geometry, this.material);
 
-    document.body.appendChild(this._renderer.domElement);
+        // document.body.appendChild(this._renderer.domElement);
+        this._view.appendChild(this._renderer.domElement);
 
-    // this._scene.add(this.cube);
-    this._camera.position.z = 5;
-    this._camera.position.y = 1;
+        // this._scene.add(this.cube);
+        this._camera.position.z = 5;
+        this._camera.position.y = 1;
 
-    const controls = new OrbitControls(this._camera, this._renderer.domElement);
-    controls.update();
-  }
+        this._controls = new OrbitControls(this._camera, this._renderer.domElement);
+    }
 
-  public resize() {
-    this._renderer.setSize(window.innerWidth, window.innerHeight);
-  }
+    public resize() {
+        this._renderer.setSize(window.innerWidth, window.innerHeight);
+    }
 
-  public update() {
-    this.rotate();
-    this._renderer.render(this._scene, this._camera);
-  }
+    public update() {
+        this.rotate();
+        this._renderer.render(this._scene, this._camera);
+        this._controls.update();
+    }
 
-  public rotate() {
-    // this.cube.rotation.x += 0.01;
-    // this.cube.rotation.y += 0.01;
-  }
+    public rotate() {
+        // this.cube.rotation.x += 0.01;
+        // this.cube.rotation.y += 0.01;
+    }
 
-  public updateTest() {
-    this.update = this.update.bind(this);
-    this._renderer.setAnimationLoop(this.update);
-  }
+    public updateTest() {
+        this.update = this.update.bind(this);
+        this._renderer.setAnimationLoop(this.update);
+    }
 
-  public addBuilding(building: Building) {
-    this._scene.add(building.buildingBox);
-  }
+    public addBuilding(building: Building) {
+        this._scene.add(building.buildingBox);
+    }
 }
